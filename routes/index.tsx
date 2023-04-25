e0adf3559b299962c2dd05b34dee9964c71d9fd0
Isaac Graves <igraves@hbs.edu>
2023-04-25 11:17:54 -0700

    test 1

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,15 +8,8 @@ export default function Home() {
         <title>Fresh App</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
+        <img src="/logo.svg" class="w-32 h-32" alt="the fresh logo: a sliced lemon dripping with juice" />
+        <p class="my-6">test 1</p>
         <Counter start={3} />
       </div>
     </>
