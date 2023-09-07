f1c0279ec19339ef6401d06350bdbbf89f52af4d
Isaac Graves <igraves@hbs.edu>
2023-09-07 14:57:31 -0700

    add hono and bun

--- src/contentful/edit-attributes.ts
+++
@@ -1,8 +0,0 @@
-import type { ContentfulEntry } from "../types";
-
-export function editAttributes(entry: ContentfulEntry, field: string) {
-  return {
-    "data-contentful-entry-id": entry.sys.id,
-    "data-contentful-field-id": field
-  };
-}
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.html(
+    `<html>
+      <body>
+        <h1>Hello, World!</h1>
+      </body>
+    </html>`
+  );
+});
+
+export default app;
