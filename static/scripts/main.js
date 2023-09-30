62c62924468e55d23c111c6968759a36bfc1c846
Isaac Graves <igraves@hbs.edu>
2023-09-30 10:55:14 +0200

    scroll animations

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,15 @@
 console.log("hello, world");
+
+// const title = document.querySelector("h1.title");
+
+function addScrollClassName() {
+  if (globalThis.scrollY > 100) {
+    document.body.classList.add("scroll");
+  } else {
+    document.body.classList.remove("scroll");
+  }
+}
+
+addScrollClassName();
+
+globalThis.addEventListener("scroll", addScrollClassName);
