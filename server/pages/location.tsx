d4df5d8051477ed1051446c73836d8a8dc3fe33c
Isaac Graves <igraves@hbs.edu>
2023-10-07 11:04:15 +0200

    update location styles

--- server/pages/location.tsx
+++ server/pages/location.tsx
@@ -1,9 +1,8 @@
 import { getImages } from "../contentful/get-images.ts";
 import { image } from "../contentful/image.ts";
 import { query } from "../contentful/query.ts";
-import { resolveAsset } from "../contentful/resolve.ts";
 import { Document } from "../templates/document.tsx";
-import { Album, ContentfulAsset, Location } from "../types.ts";
+import { Location } from "../types.ts";
 import { css } from "../utils.tsx";
 import { error404 } from "./404.tsx";
 
@@ -14,7 +13,6 @@ export async function location(slug: string) {
   });
 
   const location = locations.items[0];
-
   if (!location) return error404();
 
   const images = await getImages(location);
@@ -22,35 +20,24 @@ export async function location(slug: string) {
   return (
     <Document title={location.fields.title}>
       {css`
-        main {
-          margin: auto;
-          max-width: 700px;
-          padding: 0.25em;
-        }
-
         h2 {
-          font-size: 1.5em;
-          margin-bottom: 0.25em;
+          font-size: 1.3em;
+          margin: 1em 0.25em 0.5em 0.25em;
           border-bottom: 1px solid rgb(var(--c-vanilla));
         }
 
         section {
-          gap: 0.25em;
-          display: grid;
-          grid-template-columns: repeat(2, 1fr);
+          display: flex;
+          flex-flow: column;
+          gap: 0.5em;
         }
 
         img {
-          border-radius: 4px;
-        }
-
-        button {
-          grid-column: span 1;
-          grid-row: span 1;
+          border-radius: 0.5em;
         }
       `}
 
-      <main>
+      <div id="container">
         <h2>{location.fields.title}</h2>
 
         <section>
@@ -58,23 +45,19 @@ export async function location(slug: string) {
             const data = image(asset, { ratio: "3:4", width: 800 });
             if (!data) return null;
 
-            const full = image(asset, { width: 1920 });
-
             return (
-              <a href={full?.src}>
-                <img
-                  key={i}
-                  loading="lazy"
-                  src={data.src}
-                  width={data.width + "px"}
-                  height={data.height + "px"}
-                  style={{ backgroundImage: `url(${data.placeholder})` }}
-                />
-              </a>
+              <img
+                key={i}
+                loading="lazy"
+                src={data.src}
+                width={data.width + "px"}
+                height={data.height + "px"}
+                style={{ backgroundImage: `url(${data.placeholder})` }}
+              />
             );
           })}
         </section>
-      </main>
+      </div>
     </Document>
   );
 }
