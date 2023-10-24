4a9ee952d126797ec5f20350eea29d66e0e04c5a
Isaac Graves <igraves@hbs.edu>
2023-10-24 20:46:19 +0200

    check for preview on cache

--- src/contentful/contentful-graphql.ts
+++ src/contentful/contentful-graphql.ts
@@ -15,12 +15,15 @@ export async function contentfulGraphFetch<T>(query: string) {
   const token = preview ? import.meta.env.VITE_PREVIEW_TOKEN : import.meta.env.VITE_DELIVERY_TOKEN;
 
   const cacheKey = JSON.stringify(query);
-  const cached = sessionStorage.getItem(cacheKey);
 
-  if (cached) {
-    const { result, ts } = JSON.parse(cached) as CachedDate<T>;
-    if (ts > Date.now() - 1000 * 60 * 10) return result;
-    else sessionStorage.removeItem(cacheKey);
+  if (!preview) {
+    const cached = sessionStorage.getItem(cacheKey);
+
+    if (cached) {
+      const { result, ts } = JSON.parse(cached) as CachedDate<T>;
+      if (ts > Date.now() - 1000 * 60 * 10) return result;
+      else sessionStorage.removeItem(cacheKey);
+    }
   }
 
   const url = new URL(
@@ -39,7 +42,7 @@ export async function contentfulGraphFetch<T>(query: string) {
 
   const result = (await res.json()) as GraphQLResponse<T>;
 
-  if (result.data) {
+  if (!preview && result.data) {
     sessionStorage.setItem(cacheKey, JSON.stringify({ ts: Date.now(), result }));
   }
 
