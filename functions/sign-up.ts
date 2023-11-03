d89d37517329dbe50cf2a63759bddc656954ccdf
Isaac Graves <igraves@hbs.edu>
2023-11-03 07:51:08 +0100

    add env

--- functions/sign-up.ts
+++ functions/sign-up.ts
@@ -1,3 +1,22 @@
-export const onRequest: PagesFunction = (context) => {
-  return new Response("Hello, world!");
+import * as contentful from "contentful-management";
+
+interface Env {
+  CONTENTFUL_ACCESS_TOKEN: string;
+  VITE_SPACE_ID: string;
+  VITE_PREVIEW_TOKEN: string;
+  VITE_DELIVERY_TOKEN: string;
+}
+
+export const onRequest: PagesFunction<Env> = async (ctx) => {
+  const client = contentful.createClient({ accessToken: ctx.env.CONTENTFUL_ACCESS_TOKEN });
+
+  const space = await client.getSpace(ctx.env.VITE_SPACE_ID);
+  const env = await space.getEnvironment("master");
+
+  return new Response(JSON.stringify(env.sys), {
+    status: 200,
+    headers: {
+      "content-type": "application/json"
+    }
+  });
 };
