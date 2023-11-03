1509d8bde9fadecaf1dcb473897ced37c6eabe57
Isaac Graves <igraves@hbs.edu>
2023-11-03 08:02:14 +0100

    add custom adapter

--- functions/sign-up.ts
+++ functions/sign-up.ts
@@ -8,7 +8,41 @@ interface Env {
 }
 
 export const onRequest: PagesFunction<Env> = async (ctx) => {
-  const client = contentful.createClient({ accessToken: ctx.env.CONTENTFUL_ACCESS_TOKEN });
+  const client = contentful.createClient({
+    accessToken: ctx.env.CONTENTFUL_ACCESS_TOKEN,
+    adapter: async (config) => {
+      const url = new URL(`${config.baseURL}/${config.url}`);
+
+      if (config.params) {
+        for (const key of Object.keys(config.params)) {
+          url.searchParams.append(key, config.params[key]);
+        }
+      }
+
+      const request = new Request(url.href, {
+        method: config.method ? config.method.toUpperCase() : "GET",
+        headers: config.headers ? config.headers : {},
+        body: config.data,
+        redirect: "manual"
+      });
+
+      const response = await fetch(request);
+
+      const headers: Record<string, string> = {};
+      response.headers.forEach((value, key) => {
+        headers[key] = value;
+      });
+
+      return {
+        data: await response.json(),
+        status: response.status,
+        statusText: response.statusText,
+        headers,
+        config: config,
+        request: request
+      };
+    }
+  });
 
   const space = await client.getSpace(ctx.env.VITE_SPACE_ID);
   const env = await space.getEnvironment("master");
